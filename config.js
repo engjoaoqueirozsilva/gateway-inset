d422c27b68b8cf6abac18d7ff122d55f5bce0962
João Queiroz e Silva <155579899+engjoaoqueirozsilva@users.noreply.github.com>
2025-07-02 16:23:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const mongoURI = process.env.MONGO_URI || 'mongodb://root:DWX6i77QlWItkQpqVyPvTDDwvMFs4ime1ARHoadMXl1rruAx1pg91VOTGeyTUow3@147.93.68.31:17500/?directConnection=true';
-export const port = process.env.PORT || 3001;
+export const port = process.env.PORT || 3000;
