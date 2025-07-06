d1e7f6316c3b5eb75f759c74f791af69d1d747a0
João Queiroz e Silva <eng.joao.queiroz.silva@gmail.com>
2025-07-06 00:07:02 -0300

    Ajustes Api Key

--- middlewares/checkApiKey.js
+++ middlewares/checkApiKey.js
@@ -1,10 +1,10 @@
 export const checkApiKey = (req, res, next) => {
-  if (req.method === 'OPTIONS') { // ESSENCIAL para CORS e Preflight
+  if (req.method === 'OPTIONS') { 
     return next();
   }
   const apiKey = req.headers['x-api-key'];
-  if (!apiKey || apiKey !== '0c4d8a7a-bde6-4e3a-a2ef-5cde95727e2e') { // Usando a chave hardcoded para comparação
-    return res.status(401).json({ message: 'Acesso não autorizado: Chave de API inválida ou ausente.' });
+  if (!apiKey || apiKey !== '0c4d8a7a-bde6-4e3a-a2ef-5cde95727e2e') { 
+    return res.status(401).json({ message: 'Acesso não autorizado: Chave de API inválida ou ausente. JK' });
   }
   next();
 };
