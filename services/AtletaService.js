6ee8098055d6719dcbccccef1913d7902a7915ab
João Queiroz e Silva <eng.joao.queiroz.silva@gmail.com>
2025-07-06 11:35:33 -0300

    Ajuste de import

--- services/AtletaService.js
+++ services/AtletaService.js
@@ -1,5 +1,5 @@
 // services/AtletaService.js
-import { BaseService } from "../base/BaseService.js";
+import { BaseService } from "./base/BaseService.js";
 import { Atleta } from "../models/AtletaModel.js";
 
 class AtletaService extends BaseService {
