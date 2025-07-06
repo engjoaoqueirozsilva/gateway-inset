d7ed9fb1c9786ca37ec0430f6106c866ffe93540
João Queiroz e Silva <eng.joao.queiroz.silva@gmail.com>
2025-07-06 18:11:41 -0300

    Include Camisa no Plano

--- models/PlanoModel.js
+++ models/PlanoModel.js
@@ -3,12 +3,27 @@ import { baseFields, baseOptions } from "./base/BaseModel.js";
 
 const ParticipanteSchema = new mongoose.Schema({
   id: String,
-  nome: String
+  nome: String,
+  posicao: String,
+  camisa: {
+    type: String,
+    maxlength: 3,
+    validate: {
+      validator: function (v) {
+        return /^\d{1,3}$/.test(v); // Apenas números de até 3 dígitos
+      },
+      message: "O número da camisa deve ter no máximo 3 dígitos numéricos."
+    }
+  }
 }, { _id: false });
 
 const PlanoSchema = new mongoose.Schema({
   nome: { type: String, required: true },
-  modalidade: { type: mongoose.Schema.Types.ObjectId, ref: "Modalidade", required: true },
+  modalidade: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Modalidade",
+    required: true
+  },
   fundamentos: { type: [String], default: [] },
   participantes: { type: [ParticipanteSchema], default: [] },
   ...baseFields
