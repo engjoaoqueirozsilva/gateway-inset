cf432f9c6b77043063469b4f9beec72154d1fe1f
João Queiroz e Silva <eng.joao.queiroz.silva@gmail.com>
2025-07-06 18:09:12 -0300

    Include Camisa

--- models/AtletaModel.js
+++ models/AtletaModel.js
@@ -6,6 +6,16 @@ const AtletaSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   posicaoPreferencial: { type: String, required: true },
   posicaoSecundaria: { type: String },
+  camisa: {
+    type: String,
+    maxlength: 3,
+    validate: {
+      validator: function (v) {
+        return /^\d{1,3}$/.test(v); // Apenas números com até 3 dígitos
+      },
+      message: "O número da camisa deve ter no máximo 3 dígitos numéricos."
+    }
+  },
   modalidade: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Modalidade",
